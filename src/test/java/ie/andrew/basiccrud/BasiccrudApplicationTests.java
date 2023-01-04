88cd1148b1e6ceeb5b97cf1aca17e2b9d494ff36
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2023-01-04 09:20:24 +0000

    First commit

---
+++ src/test/java/ie/andrew/basiccrud/BasiccrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.andrew.basiccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiccrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
