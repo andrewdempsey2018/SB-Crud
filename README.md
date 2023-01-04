295c8bf1ec8ed872d5cf2d17ce3dab15ce3181be
andrewdempsey2018 <andrewdempsey2016@gmail.com>
2023-01-04 17:36:59 +0000

    add basic thyleaf templating to index page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Basic Spring Boot CRUD application
+
+![img_beta](readme_files/img_beta.png)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,10 @@
 -- Some test data for insertion into the mem database for testing upon startup
-INSERT INTO POST (ID, POST_TEXT) VALUES (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing occaecborum.');
-INSERT INTO POST (ID, POST_TEXT) VALUES (2, 'Dance the hempen jig Nelsons folly chantey flogging');
-INSERT INTO POST (ID, POST_TEXT) VALUES (3, 'Boom starboard interloper brig yard long clothes');
+
+CREATE TABLE POST_TABLE (
+    ID int,
+    POST_TEXT varchar
+);
+
+INSERT INTO POST_TABLE (ID, POST_TEXT) VALUES (1, 'Lorem ipsum dolor sit amet, consectetur adipiscing occaecborum.');
+INSERT INTO POST_TABLE (ID, POST_TEXT) VALUES (2, 'Dance the hempen jig Nelsons folly chantey flogging');
+INSERT INTO POST_TABLE (ID, POST_TEXT) VALUES (3, 'Boom starboard interloper brig yard long clothes');
